3c0100df525cc2cda7d35a42589d71a539cda172
Gurly08 <bagas.dj89@gmail.com>
2024-01-04 22:13:04 +0700

    first commit

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:reproed/ui/home/widget/boxpoint.dart';
+import 'package:reproed/ui/home/widget/boxprofile.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,172 +10,58 @@ class Home extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[100],
-        body: SingleChildScrollView(
-          child: Column(children: [
-            // ignore: sized_box_for_whitespace
+        body: ListView(
+          children: [
             Stack(
-              clipBehavior: Clip.none,
               children: [
+                //post banner banground
+                // ignore: sized_box_for_whitespace
                 Container(
                   height: 300,
                   width: 400,
-                  decoration: BoxDecoration(
-                    color: Colors.blue[200],
-                    borderRadius: const BorderRadius.only(
-                      bottomRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
-                  ),
-                  child: Image.asset(
-                    'assets/images/cptframe.png',
-                    repeat: ImageRepeat.noRepeat,
-                    fit: BoxFit.cover,
-                  ),
+                  child: Image.asset('assets/images/cptframe.png',
+                      fit: BoxFit.cover, repeat: ImageRepeat.noRepeat),
                 ),
 
-                const Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 25, left: 25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                //profile konten
+                // ignore: avoid_unnecessary_containers
+                Container(
+                  child: Column(
+                    children: [
+                      //emohty gap
+                      const SizedBox(height: 64),
+
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                        child: const Center(
+                          child: Row(
                             children: [
-                              Text(
-                                'Hi!',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                'Bagas',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
+                              Text('Hi! Bagas'),
+                              CircleAvatar(
+                                foregroundImage: AssetImage('assets/images/ppaku.png'),
                               ),
                             ],
                           ),
-                          CircleAvatar(
-                            foregroundImage:
-                                AssetImage('assets/images/ppaku.jpg'),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                ),
 
-                Positioned.fill(
-                    bottom: -250,
-                    left: 25,
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Row(
+                      //card user
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                        child: const Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Container(
-                              height: 90,
-                              width: 160,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(12),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.2),
-                                    spreadRadius: 2,
-                                    blurRadius: 5,
-                                    offset: const Offset(0,
-                                        5), // atur offset shadow sesuai kebutuhan
-                                  ),
-                                ],
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.only(left: 12),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Bagas Djunaedi',
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.black),
-                                    ),
-                                    SizedBox(height: 8),
-                                    Text(
-                                      '8',
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.black),
-                                    ),
-                                    SizedBox(height: 8),
-                                    Text(
-                                      '17 Tahun',
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                                      
-                              Padding(
-                                padding: const EdgeInsets.only(right: 25),
-                                child: Container(
-                                  height: 90,
-                                  width: 100,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(12),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black.withOpacity(0.2),
-                                      ),
-                                    ]
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        '285',style: TextStyle(
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.green[600],
-                                        ),
-                                      ),
-                                      const Text(
-                                        'Point', style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-
+                            BoxProfile(),
+                            BoxPoint(),
                           ],
                         ),
                       ),
-                    ),
-
-                ],
-              ),
-            ],
-          ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
